8ae2d9948e1d2c36bd8b7799c023581f7cad56b2
Noud63 <noudvandun@gmail.com>
2023-12-21 07:42:29 +0100

    test

--- app/page.tsx
+++ app/page.tsx
@@ -18,10 +18,9 @@ export default async function Home({ searchParams }: { searchParams: any }) {
 
   return (
     <main className="overflow-hidden">
+    
       <Hero />
-
       <HeroButtons />
-
       <TheseToo />
 
       <div className="padding-x max-width mt-28 max-md:mt-16" id="discover">
